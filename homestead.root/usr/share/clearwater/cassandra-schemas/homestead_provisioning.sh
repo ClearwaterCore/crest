65b1dc63aa68ea7ab149dced840ee8da7a40677a
root <root@ubuntu.(none)>
2015-01-06 05:15:57 -0800

    Rebase to release-60

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+keyspace=$(basename $0|sed -e 's#^\(.*\)[.]sh$#\1#')
+
 . /etc/clearwater/config
 if [ ! -z $signaling_namespace ]
 then
@@ -11,43 +13,105 @@ then
   namespace_prefix="ip netns exec $signaling_namespace"
 fi
 
-header="Waiting for Cassandra"
-let "cnt=0"
-$namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-while [ $? -ne 0 ]; do
-  sleep 1
-  printf "${header}."
-  header=""
-  let "cnt=$cnt + 1"
-  if [ $cnt -gt 120 ]; then
-    printf "*** ERROR: Cassandra did not come online!\n"
-    exit 1
-  fi
-  $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-done
-let "cnt=0"
-$namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-while [ $? -ne 0 ]; do
-  sleep 1
-  printf "${header}+"
-  header=""
-  let "cnt=$cnt + 1"
-  if [ $cnt -gt 120 ]; then
-    printf "*** ERROR: Cassandra did not come online!\n"
-    exit 1
-  fi
-  $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-done
+if [[ ! -e /var/lib/cassandra/data/${keyspace} ]]; then
+    header="Waiting for Cassandra"
+    let "cnt=0"
+    $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
+    while [ $? -ne 0 ]; do
+	sleep 1
+	printf "${header}."
+	header=""
+	let "cnt=$cnt + 1"
+	if [ $cnt -gt 120 ]; then
+	    printf "*** ERROR: Cassandra did not come online!\n"
+	    exit 1
+	fi
+	$namespace_prefix netstat -na | grep -q ":7199[^0-9]"
+    done
+    let "cnt=0"
+    $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
+    while [ $? -ne 0 ]; do
+	sleep 1
+	printf "${header}+"
+	header=""
+	let "cnt=$cnt + 1"
+	if [ $cnt -gt 120 ]; then
+	    printf "*** ERROR: Cassandra did not come online!\n"
+	    exit 1
+	fi
+	$namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
+    done
 
-if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]];
-then
-    echo "CREATE KEYSPACE homestead_provisioning WITH strategy_class='org.apache.cassandra.locator.SimpleStrategy' AND strategy_options:replication_factor=2;
-USE homestead_provisioning;
+    # Determine the redundancy strategy
+    yaml=/etc/cassandra/cassandra.yaml
+    let max_replicas=20
+    cassandra_nodes=($(cat ${yaml} | grep "[^#]*seeds:" |
+	    sed -e 's#^.*seeds:[ 	]*"\([^"]*\).*$#\1#'|sed -e 's#,# #g'))
+    snitch=$(grep "^[[:space:]]*endpoint_snitch:" ${yaml}|
+	sed -e 's#endpoint_snitch:[[:space:]]*\([^[:space:]]*\).*#\1#')
+
+    if [ "$snitch" == "PropertyFileSnitch" ]; then
+	IFS=$'\r\n' GLOBIGNORE='*' :; topology=(
+	    $(egrep "^[[:space:]]*([0-9]+[.][0-9]+[.][0-9]+[.][0-9]+|[0-9a-f\\:]+)[[:space:]]*=" /etc/cassandra/cassandra-topology.properties)
+	)
+
+	dcs=()
+	declare -A dc_num_nodes
+	for loc in "${topology[@]}"; do
+	    loc=( 
+		$(echo "$loc"|
+		    sed -e 's#\\##g'|sed -e 's#^[[:space:]]*\([^[:space:]]*\)[^=]*=[[:space:]]*\([^:]*\):[[:space:]]*\([^[:space:]]*\).*$#\1 \2 \3#' ) 
+	    )
+
+	    dc=${loc[1]}
+	    rack=${loc[2]}
+	    (for e in ${dcs[@]}; do [[ "$e" == "${dc}" ]] && exit 0; done; exit 1)
+	    if [ $? -ne 0 ]; then
+		dcs=( ${dcs[@]} $dc )
+	    fi
+	    if [ -z ${dc_num_nodes[$dc]} ]; then
+		let "dc_num_nodes[$dc]=0"
+	    fi
+	    let "dc_num_nodes[$dc]=${dc_num_nodes[$dc]} + 1"
+	done
+
+	# Generate cqlsh input for creating the keyspace
+	(
+	    printf "CREATE KEYSPACE \"${keyspace}\" WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', "
+	    let "i=0"
+	    while [ $i -lt ${#dcs[@]} ]; do
+	        let "num_replicas=${dc_num_nodes[${dcs[$i]}]}"
+		if [ $num_replicas -gt $max_replicas ]; then
+		    let "num_replicas=$max_replicas"
+		fi
+		if [ $i -ne 0 ]; then
+		    printf ", "
+		fi
+		printf "'${dcs[$i]}' : $num_replicas"
+		let "i=$i + 1"
+	    done
+	    printf " };\n"
+	) > /tmp/$$.cqlsh.in
+    else
+	let "num_replicas=${#cassandra_nodes[@]}"
+	if [ $num_replicas -gt $max_replicas ]; then
+	    let "num_replicas=$max_replicas"
+	fi
+	printf "CREATE KEYSPACE ${keyspace} WITH strategy_class = 'SimpleStrategy' AND strategy_options:replication_factor = 2;" > /tmp/$$.cqlsh.in
+    fi
+
+    $namespace_prefix cqlsh -3 < /tmp/$$.cqlsh.in
+    rm -f /tmp/$$.cqlsh.in
+
+    echo "
+USE ${keyspace};
 CREATE TABLE implicit_registration_sets (id uuid PRIMARY KEY, dummy text) WITH read_repair_chance = 1.0;
 CREATE TABLE service_profiles (id uuid PRIMARY KEY, irs text, initialfiltercriteria text) WITH read_repair_chance = 1.0;
 CREATE TABLE public (public_id text PRIMARY KEY, publicidentity text, service_profile text) WITH read_repair_chance = 1.0;
-CREATE TABLE private (private_id text PRIMARY KEY, digest_ha1 text) WITH read_repair_chance = 1.0;" | $namespace_prefix cqlsh -2
-fi
+CREATE TABLE private (private_id text PRIMARY KEY, digest_ha1 text) WITH read_repair_chance = 1.0;" >> /tmp/$$.cqlsh.in
 
-echo "USE homestead_provisioning;
+    $namespace_prefix cqlsh -2 < /tmp/$$.cqlsh.in
+    rm -f /tmp/$$.cqlsh.in
+fi
+echo "USE ${keyspace};
 ALTER TABLE private ADD realm text;" | $namespace_prefix cqlsh -2
