c4370743df8122757c1b80a53643bb4a300fff93
Doug Theobald <doug.theobald@metaswitch.com>
2015-09-29 11:05:23 -0700

    Factor out common code [CC-only] (CC_cassredun-82)

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -1,46 +1,21 @@
 #! /bin/bash
 
+keyspace=$(basename $0|sed -e 's#^\(.*\)[.]sh$#\1#')
 . /etc/clearwater/config
-if [ ! -z $signaling_namespace ]
-then
-  if [ $EUID -ne 0 ]
-  then
-    echo "When using multiple networks, schema creation must be run as root"
-    exit 2
-  fi
-  namespace_prefix="ip netns exec $signaling_namespace"
+if [ ! -z $signaling_namespace ]; then
+    if [ $EUID -ne 0 ]; then
+        echo "When using multiple networks, schema creation must be run as root"
+        exit 2
+    fi
+    namespace_prefix="ip netns exec $signaling_namespace"
 fi
 
-header="Waiting for Cassandra"
-let "cnt=0"
-$namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-while [ $? -ne 0 ]; do
-  sleep 1
-  printf "${header}."
-  header=""
-  let "cnt=$cnt + 1"
-  if [ $cnt -gt 120 ]; then
-    printf "*** ERROR: Cassandra did not come online!\n"
+$(dirname $0)/../bin/wait4cassandra ${keyspace}
+if [ $? -ne 0 ]; then
     exit 1
-  fi
-  $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-done
-let "cnt=0"
-$namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-while [ $? -ne 0 ]; do
-  sleep 1
-  printf "${header}+"
-  header=""
-  let "cnt=$cnt + 1"
-  if [ $cnt -gt 120 ]; then
-    printf "*** ERROR: Cassandra did not come online!\n"
-    exit 1
-  fi
-  $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-done
+fi
 
-if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]];
-then
+if [[ ! -e /var/lib/cassandra/data/${keyspace} ]]; then
     echo "CREATE KEYSPACE homestead_provisioning WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2};
 USE homestead_provisioning;
 CREATE TABLE implicit_registration_sets (id uuid PRIMARY KEY, dummy text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
